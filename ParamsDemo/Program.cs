fd65f20d06b9d711ca7d297eceb1a54e3cce8d27
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-24 18:12:54 +0530

    Add project files.

---
+++ ParamsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Student s = new Student();
+                s.Print("Anjali", "Vidyashree");
+                s.Print("Akshata", "Snehal", "Pratik");
+                s.Print("Akash", "Suraj", "Vishal", "Pankaj");
+                s.Print1(1, 2);
+                s.Print1(3, 4, 5);
+                s.Print1(8, 9, 7, 0);
+            }
+        }
+    }
+
---
+++ ParamsDemo/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsDemo
+{
+
+        public class Student
+    {
+       public string name;
+        public int id;
+
+        public void Print(params String[] name)
+        {
+            Console.WriteLine("count" + name.Length);
+            foreach (var item in name)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Print1(params int[] id) 
+        {
+            foreach(var item in id)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    }
+
